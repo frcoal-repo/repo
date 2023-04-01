1dc1c143e7a9170246b9f53a1568a7305565e1a5
Your Name <you@example.com>
2023-04-01 14:09:58 -0400

    frcoal

---
+++ repo.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+script_full_path=$(dirname "$0")
+cd $script_full_path || exit 1
+
+rm Packages Packages.bz2 Packages.xz Packages.zst Release
+# rm Release.gpg
+echo "[Repository] Generating Packages..."
+apt-ftparchive packages ./pool > Packages
+zstd -q -c19 Packages > Packages.zst
+xz -c9 Packages > Packages.xz
+bzip2 -c9 Packages > Packages.bz2
+
+echo "[Repository] Generating Release..."
+apt-ftparchive \
+		-o APT::FTPArchive::Release::Origin="frcoal" \
+		-o APT::FTPArchive::Release::Label="frcoal" \
+		-o APT::FTPArchive::Release::Suite="stable" \
+		-o APT::FTPArchive::Release::Version="1.0" \
+		-o APT::FTPArchive::Release::Codename="ios" \
+		-o APT::FTPArchive::Release::Architectures="iphoneos-arm iphoneos-arm64" \
+		-o APT::FTPArchive::Release::Components="main" \
+		-o APT::FTPArchive::Release::Description="frcoal" \
+		release . > Release
+
+echo "[Repository] Pushing Files..."
+rm CNAME
+git add .
+git commit -m "frcoal"
+git push
+echo frcoal623vobrv5c5rqp4ct4qlispcvkyd3pxgluvuvqvgzl5vrnk2id.onion > CNAME
+
+echo "[Repository] Finished"
